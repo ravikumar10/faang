68d2a40aa5a80b02ac22522e400951f5ffe05c53
Ravi Kumar <ravi.a.kumar@rakuten.com>
2020-07-21 11:50:18 +0530

    Day3 questions

---
+++ programming/src/java/problems/P3.java
@@ -0,0 +1,25 @@
+package src.java.problems;
+
+Problem Statement :
+        ```Design a class to calculate the median of a number stream. The class should have the following two methods:
+
+        'insertNum(int num): stores the number in the class
+        'findMedian(): returns the median of all numbers inserted in the class
+
+        'If the count of numbers inserted in the class is even, the median will be the average of the middle two numbers.
+
+        Example 1:
+
+        1. insertNum(3)
+        2. insertNum(1)
+        3. findMedian() -> output: 2
+        4. insertNum(5)
+        5. findMedian() -> output: 3
+        6. insertNum(4)
+        7. findMedian() -> output: 3.5
+
+
+        ```
+
+public class P3 {
+}
